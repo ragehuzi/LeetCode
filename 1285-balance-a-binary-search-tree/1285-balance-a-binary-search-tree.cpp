65bb9e0ff38ab5366b19c7c341d5a1f49f190c7f
Huzaifa Farooqui <153942275+ragehuzi@users.noreply.github.com>
2025-01-11 20:45:11 +0500

    Time: 131 ms (20.4%), Space: 119 MB (18.05%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -1,88 +1,106 @@
-
-
-class AVLNode {
+class AVLNode
+{
 public:
     int val;
     int height;
-    AVLNode* left;
-    AVLNode* right;
+    AVLNode *left;
+    AVLNode *right;
 
     AVLNode() : val(0), height(1), left(nullptr), right(nullptr) {}
     AVLNode(int x) : val(x), height(1), left(nullptr), right(nullptr) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    int getHeight(AVLNode* root) {
+    int getHeight(AVLNode *root)
+    {
         if (root == nullptr)
             return 0;
+
         return root->height;
     }
 
-    int getBalanceFactor(AVLNode* root) {
-        return getHeight(root->left) - getHeight(root->right);
+    int getBalanceFactor(AVLNode *root)
+    {
+        return (getHeight(root->left) - getHeight(root->right));
     }
 
-    void rotateRight(AVLNode*& Y) {
-        AVLNode* X = Y->left;
-        AVLNode* T2 = X->right;
+    void rotateRight(AVLNode *&Y)
+    {
+        AVLNode *X = Y->left;
+        AVLNode *T2 = X->right;
 
-        // Perform rotation
+        // Rotation
         X->right = Y;
         Y->left = T2;
 
-        // Update heights
         Y->height = 1 + max(getHeight(Y->left), getHeight(Y->right));
         X->height = 1 + max(getHeight(X->left), getHeight(X->right));
 
         Y = X;
     }
 
-    void rotateLeft(AVLNode*& X) {
-        AVLNode* Y = X->right;
-        AVLNode* T2 = Y->left;
+    void rotateLeft(AVLNode *&X)
+    {
+        AVLNode *Y = X->right;
+        AVLNode *T2 = Y->left;
 
-        // Perform rotation
         Y->left = X;
         X->right = T2;
 
-        // Update heights
         X->height = 1 + max(getHeight(X->left), getHeight(X->right));
         Y->height = 1 + max(getHeight(Y->left), getHeight(Y->right));
 
         X = Y;
     }
 
-    void balanceTree(AVLNode*& root) {
+    void balanceTree(AVLNode *&root)
+    {
         int balance = getBalanceFactor(root);
 
-        if (balance > 1) {
-            if (getBalanceFactor(root->left) >= 0) {
-                rotateRight(root); // Left-Left Case
-            } else {
-                rotateLeft(root->left); // Left-Right Case
+        if (balance > 1)
+        {
+            // Check if it is the Left-Left Case
+            if (getBalanceFactor(root->left) >= 0)
+            {
                 rotateRight(root);
             }
-        } else if (balance < -1) {
-            if (getBalanceFactor(root->right) <= 0) {
-                rotateLeft(root); // Right-Right Case
-            } else {
-                rotateRight(root->right); // Right-Left Case
+            else
+            {
+                // Left-right Case
+                rotateLeft(root->left);
+                rotateRight(root);
+            }
+        }
+        else if (balance < -1)
+        { // Right-Right Case
+            if (getBalanceFactor(root->right) <= 0)
+            {
+                rotateLeft(root);
+            }
+            else
+            {
+                // Right - Left Case
+                rotateRight(root->left);
                 rotateLeft(root);
             }
         }
     }
 
-    void addElement(AVLNode*& root, int x) {
-        AVLNode* newNode = new AVLNode(x);
-        if (root == nullptr) {
+    void addElement(AVLNode *&root, int x)
+    {
+        AVLNode *newNode = new AVLNode(x);
+        if (root == nullptr)
+        {
             root = newNode;
             return;
         }
 
         if (x < root->val)
             addElement(root->left, x);
-        else if (x > root->val)
+
+        if (x > root->val)
             addElement(root->right, x);
 
         root->height = 1 + max(getHeight(root->left), getHeight(root->right));
@@ -90,7 +108,23 @@ public:
         balanceTree(root);
     }
 
-    void inorderTraversal(TreeNode* root, vector<int>& sortedArr) {
+    void addBST(TreeNode *&root, int x)
+    {
+        TreeNode *newNode = new TreeNode(x);
+        if (root == nullptr)
+        {
+            root = newNode;
+            return;
+        }
+        if (root->val >= x)
+            addBST(root->right, x);
+
+        if (root->val < x)
+            addBST(root->right, x);
+    }
+
+    void inorderTraversal(TreeNode *root, vector<int> &sortedArr)
+    {
         if (root == nullptr)
             return;
 
@@ -99,30 +133,29 @@ public:
         inorderTraversal(root->right, sortedArr);
     }
 
-    TreeNode* convertAVLToTreeNode(AVLNode* root) {
+    TreeNode *convertAVLToTreeNode(AVLNode *root)
+    {
         if (root == nullptr)
             return nullptr;
 
-        TreeNode* newRoot = new TreeNode(root->val);
+        TreeNode *newRoot = new TreeNode(root->val);
         newRoot->left = convertAVLToTreeNode(root->left);
         newRoot->right = convertAVLToTreeNode(root->right);
 
         return newRoot;
     }
 
-    TreeNode* balanceBST(TreeNode* root) {
+    TreeNode *balanceBST(TreeNode *root)
+    {
         vector<int> nums;
 
-        // Perform an inorder traversal to get a sorted array
         inorderTraversal(root, nums);
-        root = nullptr; // Clear the original BST
+        root = nullptr;
 
-        // Build the AVL tree from the sorted array
-        AVLNode* avlRoot = nullptr;
+        AVLNode *avlroot = nullptr;
         for (auto i : nums)
-            addElement(avlRoot, i);
+            addElement(avlroot, i);
 
-        // Convert the AVL tree to a TreeNode tree
-        return convertAVLToTreeNode(avlRoot);
+        return convertAVLToTreeNode(avlroot);
     }
 };
